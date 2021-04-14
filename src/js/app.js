19883c30976831886fb6551c2feea2647ff23698
mdemiguels <miguelschz1997@gmail.com>
2021-04-14 13:30:40 +0200

    Añadidos sliders de imagenes

---
+++ src/js/app.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoader', function() {
+
+  eventListeners();
+})
+
+function eventListeners() {
+  const mobileMenu = document.querySelector('.mobile-menu');
+
+  mobileMenu.addEventListener('click', navegacionResponsive);
+}
+
+function navegacionResponsive() {
+  const navegacion = document.querySelector('.navegacion');
+
+  navegacion.classList.toggle('mostrar');
+}
+
+
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
