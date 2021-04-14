41e6eb2b6adfa6cc62749e0c4370e6229a9024da
mdemiguels <miguelschz1997@gmail.com>
2021-04-14 18:03:19 +0200

    eliminados sliders, añadido menu bocadillo a index

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
-document.addEventListener('DOMContentLoader', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
   eventListeners();
-})
+});
 
 function eventListeners() {
   const mobileMenu = document.querySelector('.mobile-menu');
@@ -13,36 +13,4 @@ function navegacionResponsive() {
   const navegacion = document.querySelector('.navegacion');
 
   navegacion.classList.toggle('mostrar');
-}
-
-
-
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
 }
