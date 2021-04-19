bb91a71d36c6780dc403d9f34e6ae23bd47f2016
mdemiguels <miguelschz1997@gmail.com>
2021-04-19 20:22:00 +0200

    añadido dark mode en todas las secciones

--- src/js/app.js
+++ src/js/app.js
@@ -6,10 +6,26 @@ document.addEventListener('DOMContentLoaded', function () {
 
 });
 
-function darkMode() {   
+function darkMode() {
+
+  const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+
+  if (prefiereDarkMode.matches) {
+    document.body.classList.add('dark-mode');
+  } else {
+    document.body.classList.remove('dark-mode');
+  }
+
+  prefiereDarkMode.addEventListener('change', function () {
+    if (prefiereDarkMode.matches) {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+  });
 
   const botonDarkMode = document.querySelector('.dark-mode-boton');
-  botonDarkMode.addEventListener('click', function() {
+  botonDarkMode.addEventListener('click', function () {
     document.body.classList.toggle('dark-mode');
   });
 
