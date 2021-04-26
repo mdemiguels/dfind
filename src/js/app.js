77004038662b7991cc898e263d9737759bbb69af
mdemiguels <miguelschz1997@gmail.com>
2021-04-26 21:29:49 +0200

    añadidos sliders y api google maps

--- src/js/app.js
+++ src/js/app.js
@@ -2,36 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
   eventListeners();
 
-  darkMode();
-
-
-
 });
-
-function darkMode() {
-
-  const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
-
-  if (prefiereDarkMode.matches) {
-    document.body.classList.add('dark-mode');
-  } else {
-    document.body.classList.remove('dark-mode');
-  }
-
-  prefiereDarkMode.addEventListener('change', function () {
-    if (prefiereDarkMode.matches) {
-      document.body.classList.add('dark-mode');
-    } else {
-      document.body.classList.remove('dark-mode');
-    }
-  });
-
-  const botonDarkMode = document.querySelector('.dark-mode-boton');
-  botonDarkMode.addEventListener('click', function () {
-    document.body.classList.toggle('dark-mode');
-  });
-
-}
 
 function eventListeners() {
   const mobileMenu = document.querySelector('.mobile-menu');
@@ -43,37 +14,4 @@ function navegacionResponsive() {
   const navegacion = document.querySelector('.navegacion');
 
   navegacion.classList.toggle('mostrar');
-}
-
-
-
-//galeria imagenes
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) { slideIndex = 1 }
-  if (n < 1) { slideIndex = slides.length }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-  captionText.innerHTML = dots[slideIndex - 1].alt;
 }
